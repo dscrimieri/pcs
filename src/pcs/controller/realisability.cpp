c2cefe8ed35a93b5292ce7b644ec20b885111916
Muhammad Talal <mtalal@bradford.ac.uk>
2022-03-25 12:06:30 +0000

    Worked done on realisibiltity

--- src/pcs/controller/realisability.cpp
+++ src/pcs/controller/realisability.cpp
@@ -1,5 +1,37 @@
 #include "pcs/controller/realisability.h"
+#include "pcs/lts/state.h"
+
 
 namespace pcs::controller {
+    Realisability::Realisability()
+    {
+
+    }
+
+    bool Realisability::checkRealisability(LabelledTransitionSystem<std::string> lts, Recipe recipe)
+    {
+        // add loop for the size of recipe states if the states of recipe are not in sequential order
+        if (lts.GetInitialState() == recipe.lts_.GetInitialState()) {
+            std::string ltsState = lts.GetInitialState();    
+            for (auto ele : recipe.lts_.states_) {
+                if (ele.first == ltsState) {
+                    for (auto transitionStates : ele.second.transitions_) {
+                        for (auto ltsTransitionStates : lts.states_[ltsState].transitions_) {
+                            if (transitionStates.second == ltsTransitionStates.second) {
+                                ltsState = ltsTransitionStates.second;
+                            }
+                            else {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
 
 }
--- src/pcs/controller/realisability.h
+++ src/pcs/controller/realisability.h
@@ -1,5 +1,21 @@
 #pragma once
 
-namespace pcs::controller {
+#include <string>
+#include <span>
+#include <vector>
 
+#include "pcs/lts/state.h"
+#include "pcs/lts/lts.h"
+#include "pcs/product/recipe.h"
+#include "pcs/controller/topology.h"
+
+namespace pcs::controller {
+	class Realisability {
+	public:
+		pcs::lts::LabelledTransitionSystem<std::string> lts;
+	private:
+		Realisability();
+		bool checkRealisability(LabelledTransitionSystem<std::string> lts, Recipe recipe);
+	};
+	
 }
